64de2ba2e47398a2782b545901ea6555829a49cb
keshiagabali <150538341+keshiagabali@users.noreply.github.com>
2024-03-16 13:19:32 +0100

    Intégration du Hero

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php get_header(); ?>
+
+<div class="hero">
+
+    <?php
+        $args_related_photos = array(
+            'post_type' => 'photo', 
+            'posts_per_page' => 1,
+            'orderby' => 'rand', 
+        );
+        $related_photos_query = new WP_Query($args_related_photos);
+
+        while ($related_photos_query->have_posts()) :
+            $related_photos_query->the_post();
+    ?>
+            <div class="hero-image" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');">
+                <img src="<?php echo get_template_directory_uri(); ?>/img_logo/Titre header.png" alt="Titre Accueil">
+            </div>
+
+        <?php endwhile; ?>
+        
+        <?php wp_reset_postdata(); ?>
+
+</div>
+
     <!-- Bouton chargez plus -->
     <div class="home-button">
         <a href="<?php echo home_url(); ?>" class="button">Chargez plus</a>
